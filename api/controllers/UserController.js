25d53d8342ae7a787741e563fbb6b3f250cfcca5
小林拓 <keep_slimbody@yahoo.co.jp>
2014-10-14 01:50:51 +0900

    viewを作ってみました

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,10 @@
  */
 
 module.exports = {
-	
+	index: function (req,res) {
+        // This will render the view: /path/to/chat_sample/views/user/index.ejs
+        //res.send( 'Hello Sails!!!' );
+        res.view({ hello: req.ip });
+    }
 };
 
