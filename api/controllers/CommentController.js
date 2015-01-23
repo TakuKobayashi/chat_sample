48fd4b0ac9f8cb197d94c3c9b6e092480d8fd13d
小林拓 <keep_slimbody@yahoo.co.jp>
2015-01-24 07:02:40 +0900

    要調整だがチャットの形は一応出来た!?

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -6,15 +6,24 @@
  */
 
 module.exports = {
+
   index: function (req, res) {
-    console.log(req.allParams);
-    User.findOne({id: req.param('user_id')}).exec(function(err, user){
-      Room.findOne({id: req.param('room_id')}).exec(function(err, room){
-        Comment.find().where({roomId: req.param('room_id')}).exec(function(err, comments){
-          console.log(comments);
-          res.view({user: user, room: room, comments: comments});
-        });
+  	console.log(req.allParams());
+  	console.log(req.socket);
+    Room.findOne({id: req.param('room_id')}).exec(function(err, room){
+      Comment.find().where({roomId: req.param('room_id')}).exec(function(err, comments){
+        //User.subscribe(req.socket);
+        //Commentの更新を通知するためにsubscribeを追加
+        Comment.subscribe(req.socket);
+        res.json(comments);
       });
+    });
+  },
+  create: function(req, res){
+  	//console.log(req.allParams());
+    Comment.create({userId: req.param('user_id'), roomId: req.param('room_id'), message: req.param('message')}).exec(function(err, comment){
+      Comment.publishCreate(comment);
+      res.json(comment)
     });
   }
 };
--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -8,9 +8,32 @@
 module.exports = {
   index: function (req,res) {
     User.findOne({id: req.param('user_id')}).exec(function(err, user){
-      var page = req.param('page') || 1;
-      Room.find().paginate({page: page, limit: 10}).exec(function(err, rooms){
-        res.view({rooms: rooms, user: user, page: page});
+      var roomId = req.param('id');
+      if(req.param('id')){
+        Room.findOne({id: req.param('id')}).exec(function(err, room){
+          Comment.find().where({roomId: req.param('id')}).exec(function(err, comments){
+            //User.subscribe(req.socket);
+            //Commentの更新を通知するためにsubscribeを追加
+            res.view("room/show.ejs", {user: user, room: room, comments: comments});
+          });
+        });
+      }else{
+        var page = req.param('page') || 1;
+        Room.find().paginate({page: page, limit: 10}).exec(function(err, rooms){
+          res.view({rooms: rooms, user: user, page: page});
+        });
+      }
+    });
+  },
+  show: function (req,res) {
+    User.findOne({id: req.param('user_id')}).exec(function(err, user){
+      console.log(req.allParams());
+      Room.findOne({id: req.param('id')}).exec(function(err, room){
+        Comment.find().where({roomId: req.param('id')}).exec(function(err, comments){
+            Comment.subscribe(req.socket);
+            //Commentの更新を通知するためにsubscribeを追加
+            res.view({user: user, room: room, comments: comments});
+        });
       });
     });
   },
